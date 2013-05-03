ddacb7a3516594434c6cca8139d47188d70c095b
Licker Nandor <licker.nandor@gmail.com>
2013-05-03 21:10:56 +0300

    Removed old files

--- maze/MZEntity.cc
+++
@@ -1,6 +0,0 @@
-// This file is part of the MAZE project
-// Licensing information can be found in the LICENSE file
-// (C) 2012 The MAZE project. All rights reserved.
-
-#include "MZEntity.h"
-using namespace MAZE;
